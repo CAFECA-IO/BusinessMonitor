f65768605befbd19120b6aab7ea7b3c51082924b
julian <julian890701@gmail.com>
2025-08-05 17:23:02 +0800

    metadata

--- src/app/landing/page.tsx
+++ src/app/landing/page.tsx
@@ -1,57 +1,51 @@
-import Head from 'next/head';
 import Navbar from '@/components/common/navbar';
 import Kv from '@/components/landing_page/kv';
 import IntroCard from '@/components/landing_page/intro_card';
 import Cta from '@/components/landing_page/cta';
 import Footer from '@/components/common/footer';
 
+export const metadata = {
+  title: 'CAFECA - Own Your Identity',
+};
+
 export default function LandingPage() {
   return (
-    <>
-      <Head>
-        <meta charSet="UTF-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon/favicon.ico" />
-        <title>CAFECA - Own Your Identity</title>
-      </Head>
+    <main className="flex min-h-screen flex-col items-center">
+      <Navbar />
 
-      <main className="flex min-h-screen flex-col items-center">
-        <Navbar />
+      <Kv />
 
-        <Kv />
+      <div className="flex flex-col items-center">
+        <IntroCard
+          imgSrc="/elements/identity.png"
+          title="Verify your **identity** without relying on third parties."
+          description="Decentralized Verification: No intermediaries needed. Identity authorization is achieved through blockchain smart contract interactions."
+        />
+        <IntroCard
+          imgSrc="/elements/phone.png"
+          title="**Prove** who you are without revealing who you are."
+          description="Zero-Knowledge Proof: Uses mobile biometric authentication and the FIDO2 protocol to verify identity without revealing personal information."
+        />
+        <IntroCard
+          imgSrc="/elements/service.png"
+          title="Easily connect your identity across **apps** and **services**."
+          description="Multi-Platform Integration: Supports OAuth2 protocol, allowing easy integration across various services and applications."
+        />
+        <IntroCard
+          imgSrc="/elements/global.png"
+          title="Create once. Use **anywhere**. Stay in control."
+          description="Portable Identity: Create your identity once and use it across multiple platforms. All authentication data is stored on the user's mobile device, free from platform lock-in."
+        />
+        <IntroCard
+          imgSrc="/elements/private.png"
+          title="Your data stays yours—**private**, **encrypted**, and **protected**."
+          description="Privacy Protection: Personal data is encrypted and stored securely, inaccessible to unauthorized parties and protected from misuse."
+        />
+      </div>
 
-        <div className="flex flex-col items-center">
-          <IntroCard
-            imgSrc="/elements/identity.png"
-            title="Verify your **identity** without relying on third parties."
-            description="Decentralized Verification: No intermediaries needed. Identity authorization is achieved through blockchain smart contract interactions."
-          />
-          <IntroCard
-            imgSrc="/elements/phone.png"
-            title="**Prove** who you are without revealing who you are."
-            description="Zero-Knowledge Proof: Uses mobile biometric authentication and the FIDO2 protocol to verify identity without revealing personal information."
-          />
-          <IntroCard
-            imgSrc="/elements/service.png"
-            title="Easily connect your identity across **apps** and **services**."
-            description="Multi-Platform Integration: Supports OAuth2 protocol, allowing easy integration across various services and applications."
-          />
-          <IntroCard
-            imgSrc="/elements/global.png"
-            title="Create once. Use **anywhere**. Stay in control."
-            description="Portable Identity: Create your identity once and use it across multiple platforms. All authentication data is stored on the user's mobile device, free from platform lock-in."
-          />
-          <IntroCard
-            imgSrc="/elements/private.png"
-            title="Your data stays yours—**private**, **encrypted**, and **protected**."
-            description="Privacy Protection: Personal data is encrypted and stored securely, inaccessible to unauthorized parties and protected from misuse."
-          />
-        </div>
+      <Cta />
 
-        <Cta />
-
-        <Footer />
-      </main>
-    </>
+      <Footer />
+    </main>
   );
 }
--- src/app/search/result/page.tsx
+++ src/app/search/result/page.tsx
@@ -1,10 +1,13 @@
-import Head from 'next/head';
 import Navbar from '@/components/common/navbar';
 import Footer from '@/components/common/footer';
 import SearchArea from '@/components/common/search_area';
 import SearchResultList from '@/components/search/search_result_list';
 import Breadcrumb from '@/components/common/breadcrumb';
 import { dummyBusinesses } from '@/interfaces/business';
+
+export const metadata = {
+  title: 'CAFECA - Searching Result',
+};
 
 export default function SearchingResultPage() {
   const dummyData = {
@@ -14,43 +17,34 @@ export default function SearchingResultPage() {
   };
 
   return (
-    <>
-      <Head>
-        <meta charSet="UTF-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon/favicon.ico" />
-        <title>Business Monitor - Searching Result</title>
-      </Head>
+    <main className="flex flex-col items-center bg-surface-background">
+      <Navbar />
 
-      <main className="flex flex-col items-center bg-surface-background">
-        <Navbar />
+      <div className="flex min-h-screen w-full flex-col items-center gap-60px px-80px pb-60px pt-80px">
+        {/* Info: (20250804 - Julian) Breadcrumb  */}
+        <Breadcrumb
+          items={[
+            { name: 'Home', link: '/' },
+            { name: 'Searching Result', link: '/search/result' },
+          ]}
+        />
 
-        <div className="flex min-h-screen w-full flex-col items-center gap-60px px-80px pb-60px pt-80px">
-          {/* Info: (20250804 - Julian) Breadcrumb  */}
-          <Breadcrumb
-            items={[
-              { name: 'Home', link: '/' },
-              { name: 'Searching Result', link: '/search/result' },
-            ]}
-          />
-
-          {/* Info: (20250804 - Julian) Search Area */}
-          <div className="w-3/4">
-            <SearchArea />
-          </div>
-
-          {/* Info: (20250804 - Julian) Search Result List */}
-          <div className="w-3/4">
-            <SearchResultList
-              countOfTotal={dummyData.countOfTotal}
-              currentRow={dummyData.currentRow}
-              list={dummyData.businesses}
-            />
-          </div>
+        {/* Info: (20250804 - Julian) Search Area */}
+        <div className="w-3/4">
+          <SearchArea />
         </div>
 
-        <Footer />
-      </main>
-    </>
+        {/* Info: (20250804 - Julian) Search Result List */}
+        <div className="w-3/4">
+          <SearchResultList
+            countOfTotal={dummyData.countOfTotal}
+            currentRow={dummyData.currentRow}
+            list={dummyData.businesses}
+          />
+        </div>
+      </div>
+
+      <Footer />
+    </main>
   );
 }
