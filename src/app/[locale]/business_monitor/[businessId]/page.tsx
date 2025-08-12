0aefd6879a1b94f9677bfb4ecac3f43588ae2eda
julian <julian890701@gmail.com>
2025-08-12 15:27:28 +0800

    develop page body

--- src/app/[locale]/business_monitor/[businessId]/page.tsx
+++ src/app/[locale]/business_monitor/[businessId]/page.tsx
@@ -1,6 +1,4 @@
-import TabBar from '@/components/business/tab_bar';
-import Layout from '@/components/common/layout';
-import { BM_URL } from '@/constants/url';
+import BusinessDetailPageBody from '@/components/business/business_detail_page_body';
 
 export const metadata = {
   title: 'CAFECA - Business Detail',
@@ -14,20 +12,5 @@ interface IBusinessDetailPageProps {
 export default async function BusinessDetailPage({ params }: IBusinessDetailPageProps) {
   const { businessId } = await params;
 
-  const crumbsItems = [
-    { name: 'Home', link: BM_URL.HOME },
-    { name: 'Searching Result', link: BM_URL.SEARCH },
-    { name: businessId, link: '' }, // ToDo: (20250811 - Julian) 應改為 Business name
-  ];
-
-  return (
-    <Layout
-      crumbsItems={crumbsItems}
-      pageBgColor="bg-surface-background"
-      className="gap-60px px-80px"
-    >
-      {/* Info: (20250811 - Julian) Tab Bar */}
-      <TabBar />
-    </Layout>
-  );
+  return <BusinessDetailPageBody businessId={businessId} />;
 }
---
+++ src/components/business/business_detail_page_body.tsx
@@ -0,0 +1,38 @@
+'use client';
+
+import React, { useState } from 'react';
+import TabBar from '@/components/business/tab_bar';
+import Layout from '@/components/common/layout';
+import { BM_URL } from '@/constants/url';
+import { TAB_BAR_ITEMS, TabBarItem } from '@/constants/tab_bar';
+
+interface IBusinessDetailPageProps {
+  businessId: string;
+}
+
+const BusinessDetailPageBody: React.FC<IBusinessDetailPageProps> = ({ businessId }) => {
+  const [currentTab, setCurrentTab] = useState<TabBarItem>(TAB_BAR_ITEMS[0]);
+
+  const crumbsItems = [
+    { name: 'Home', link: BM_URL.HOME },
+    { name: 'Searching Result', link: BM_URL.SEARCH },
+    { name: businessId, link: '' }, // ToDo: (20250811 - Julian) 應改為 Business name
+  ];
+
+  const onTabChange = (tab: TabBarItem) => {
+    setCurrentTab(tab);
+  };
+
+  return (
+    <Layout
+      crumbsItems={crumbsItems}
+      pageBgColor="bg-surface-background"
+      className="gap-60px px-80px"
+    >
+      {/* Info: (20250811 - Julian) Tab Bar */}
+      <TabBar currentTab={currentTab} onTabChange={onTabChange} />
+    </Layout>
+  );
+};
+
+export default BusinessDetailPageBody;
