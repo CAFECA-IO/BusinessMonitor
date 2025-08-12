afcf5744e7c09361326778036878772815648791
julian <julian890701@gmail.com>
2025-08-12 18:35:33 +0800

    develop tooltip

--- src/components/business/info_block_layout.tsx
+++ src/components/business/info_block_layout.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Image from 'next/image';
+import Tooltip from '@/components/common/tooltip';
 
 interface IInfoBlockLayoutProps {
   title: string;
@@ -14,20 +14,7 @@ const InfoBlockLayout: React.FC<IInfoBlockLayoutProps> = ({
   tooltipContent = '',
   className = '',
 }) => {
-  /* ToDo: (20250812 - Julian) Tooltip */
-  const tooltip = tooltipContent && (
-    <div className="relative flex items-center">
-      <Image
-        src="/icons/info.svg"
-        width={24}
-        height={24}
-        alt="info_icon"
-        className="hover:cursor-pointer"
-      />
-
-      <div className=""></div>
-    </div>
-  );
+  const tooltip = tooltipContent && <Tooltip content={tooltipContent} direction="right" />;
 
   return (
     <div className="flex h-420px flex-col gap-40px rounded-radius-l bg-white px-60px py-40px">
