c90fb73880d4c157e11a8a55cab96f3bf30f85ca
Tzuhan <tzuhan@CAFECAdeMacBook-Air.local>
2025-08-12 15:36:33 +0800

    chore: fix jest testPathPatterns & pre-commit

--- src/__tests__/helpers/next-server.ts
+++ src/__tests__/helpers/next-server.ts
@@ -2,7 +2,7 @@ import http from 'node:http';
 import next from 'next';
 
 export async function createNextTestServer() {
-  const app = next({ dev: false });
+  const app = next({ dev: true });
   await app.prepare();
   const handle = app.getRequestHandler();
   const server = http.createServer((req, res) => handle(req, res));
