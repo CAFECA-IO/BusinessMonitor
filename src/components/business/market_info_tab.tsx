86de5907232b378b9d1c90da2bfec6f740fd338c
julian <julian890701@gmail.com>
2025-09-05 15:34:00 +0800

    add market info & operations tab translation

--- src/components/business/market_info_tab.tsx
+++ src/components/business/market_info_tab.tsx
@@ -1,6 +1,9 @@
+'use client';
+
 import React from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
+import { useTranslation } from 'react-i18next';
 import { IoTriangle } from 'react-icons/io5';
 import { FaChevronRight } from 'react-icons/fa6';
 import { INews, mockNews } from '@/interfaces/news';
@@ -28,6 +31,8 @@ const NewsItem: React.FC<INewsItemProps> = ({ news }) => {
 };
 
 const MarketInfoTab: React.FC = () => {
+  const { t } = useTranslation(['business_detail']);
+
   // ToDo: (20250826 - Julian) Fetch real stock & news data
   const {
     price,
@@ -96,41 +101,51 @@ const MarketInfoTab: React.FC = () => {
           <div className="grid flex-1 grid-cols-2 gap-40px text-sm">
             <div className="flex flex-col gap-4px">
               <div className="flex items-center justify-between">
-                <p className="font-normal text-text-secondary">Open</p>
+                <p className="font-normal text-text-secondary">
+                  {t('business_detail:MARKET_INFO_OPEN')}
+                </p>
                 <p className={`${stockColor} font-medium`}>{formatNumber(open)}</p>
               </div>
               <div className="flex items-center justify-between">
-                <p className="font-normal text-text-secondary">Low</p>
+                <p className="font-normal text-text-secondary">
+                  {t('business_detail:MARKET_INFO_LOW')}
+                </p>
                 <p className="font-medium text-text-error">{formatNumber(low)}</p>
               </div>
               <div className="flex items-center justify-between">
-                <p className="font-normal text-text-secondary">High</p>
+                <p className="font-normal text-text-secondary">
+                  {t('business_detail:MARKET_INFO_HIGH')}
+                </p>
                 <p className="font-medium text-text-success">{formatNumber(high)}</p>
               </div>
               <div className="flex items-center justify-between text-text-secondary">
-                <p className="font-normal">52 wk high</p>
+                <p className="font-normal">
+                  {t('business_detail:MARKET_INFO_FIFTY_TWO_WEEK_HIGH')}
+                </p>
                 <p className="font-medium">{formatNumber(fiftyTwoWeekHigh)}</p>
               </div>
               <div className="flex items-center justify-between text-text-secondary">
-                <p className="font-normal">52 wk low</p>
+                <p className="font-normal">{t('business_detail:MARKET_INFO_FIFTY_TWO_WEEK_LOW')}</p>
                 <p className="font-medium">{formatNumber(fiftyTwoWeekLow)}</p>
               </div>
             </div>
             <div className="flex flex-col gap-4px">
               <div className="flex items-center justify-between text-text-secondary">
-                <p className="font-normal">Avg Vol (3M)</p>
+                <p className="font-normal">
+                  {t('business_detail:MARKET_INFO_AVG_VOLUME_THREE_MONTH')}
+                </p>
                 <p className="font-medium">{formatNumber(avgVolume3Month)}</p>
               </div>
               <div className="flex items-center justify-between text-text-secondary">
-                <p className="font-normal">Shares Outstanding</p>
+                <p className="font-normal">{t('business_detail:MARKET_INFO_SHARES_OUTSTANDING')}</p>
                 <p className="font-medium">{formatNumber(sharesOutstanding)}</p>
               </div>
               <div className="flex items-center justify-between text-text-secondary">
-                <p className="font-normal">Mkt Cap</p>
+                <p className="font-normal">{t('business_detail:MARKET_INFO_MKT_CAP')}</p>
                 <p className="font-medium">{formatNumber(mktCap)}</p>
               </div>
               <div className="flex items-center justify-between text-text-secondary">
-                <p className="font-normal">Div Yield</p>
+                <p className="font-normal">{t('business_detail:MARKET_INFO_DIV_YIELD')}</p>
                 <p className="font-medium">{formatNumber(divYield)}%</p>
               </div>
             </div>
@@ -142,11 +157,17 @@ const MarketInfoTab: React.FC = () => {
 
         {/* Info: (20250826 - Julian) Traders’ Sentiment */}
         <div className="flex flex-col gap-24px">
-          <p className="font-medium text-text-secondary">Traders’ Sentiment</p>
+          <p className="font-medium text-text-secondary">
+            {t('business_detail:TRADERS_SENTIMENT_TITLE')}
+          </p>
           <div className="flex flex-col gap-8px">
             <div className="flex items-center justify-between text-base font-medium">
-              <p className="text-text-success">{sellersPercent}% Sellers</p>
-              <p className="text-text-error">{buyersPercent}% Buyers</p>
+              <p className="text-text-success">
+                {sellersPercent}% {t('business_detail:TRADERS_SENTIMENT_SELLERS')}
+              </p>
+              <p className="text-text-error">
+                {buyersPercent}% {t('business_detail:TRADERS_SENTIMENT_BUYERS')}
+              </p>
             </div>
             <div className="relative h-10px w-full overflow-hidden rounded-full bg-text-error py-4px">
               <span
@@ -160,7 +181,7 @@ const MarketInfoTab: React.FC = () => {
 
       {/* Info: (20250826 - Julian) News Part */}
       <div className="flex flex-col gap-40px">
-        <p className="text-h5 font-bold text-text-brand">News</p>
+        <p className="text-h5 font-bold text-text-brand">{t('business_detail:NEWS_TITLE')}</p>
         <hr className="h-px border-border-secondary" />
         <div className="flex flex-col gap-24px">{newsList}</div>
         {/* ToDo: (20250826 - Julian) Link to news page */}
@@ -168,7 +189,7 @@ const MarketInfoTab: React.FC = () => {
           href={'/'}
           className="flex items-center justify-center gap-8px text-base font-normal text-button-link hover:underline"
         >
-          <p>See More</p>
+          <p>{t('business_detail:NEWS_SEE_MORE')}</p>
           <FaChevronRight size={18} />
         </Link>
       </div>
