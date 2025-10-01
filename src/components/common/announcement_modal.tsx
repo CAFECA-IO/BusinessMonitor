40f3dded044178570323039603f0c26f2f375664
julian <julian890701@gmail.com>
2025-10-01 18:11:30 +0800

    connect announcement api

--- src/components/common/announcement_modal.tsx
+++ src/components/common/announcement_modal.tsx
@@ -5,9 +5,33 @@ import { LuTrash2 } from 'react-icons/lu';
 import { RxCross1 } from 'react-icons/rx';
 import { TbEdit } from 'react-icons/tb';
 import Button from '@/components/common/button';
+import { IAnnouncementItem } from '@/interfaces/announcement';
+import { timestampToString } from '@/lib/common';
 
-const AnnouncementModal: React.FC = () => {
+interface IAnnouncementModalProps {
+  announcement: IAnnouncementItem | null;
+  visibleHandler: () => void;
+}
+
+// const DeleteModal: React.FC = () => {
+//   return <div>DeleteModal</div>;
+// };
+
+const AnnouncementModal: React.FC<IAnnouncementModalProps> = ({ announcement, visibleHandler }) => {
+  if (!announcement) return null;
+
+  const { title, date, content, imageUrl } = announcement;
+
   const modalType = 'Announcement';
+
+  const timestamp = new Date(date).getTime() / 1000;
+  const formattedTime = timestampToString(timestamp);
+
+  const isShowImage = imageUrl && (
+    <div className="relative h-250px w-800px">
+      <Image src={imageUrl} alt="announcement_image" objectFit="cover" fill />
+    </div>
+  );
 
   return (
     <div className="fixed left-0 top-0 z-50 flex size-full flex-col items-center justify-center bg-black/50">
@@ -15,20 +39,22 @@ const AnnouncementModal: React.FC = () => {
         {/* Info: (20251001 - Julian) Modal Type */}
         <div className="flex items-center">
           <p className="flex-1 text-center text-lg font-bold text-text-primary">{modalType}</p>
-          <button type="button" className="p-10px text-text-primary">
+          <button type="button" onClick={visibleHandler} className="p-10px text-text-primary">
             <RxCross1 size={24} />
           </button>
         </div>
 
-        <hr className="border-border-secondary" />
+        <hr className="bg-border-secondary" />
 
         <div className="flex h-500px flex-col gap-24px overflow-y-auto">
           {/* Info: (20251001 - Julian) Main Title */}
-          <div className="relative flex flex-col items-center justify-center gap-24px">
-            <h4 className="text-h4 font-bold">Exciting News Ahead!</h4>
-            <p className="text-sm font-bold text-button-secondary">2025-06-13 14:06</p>
+          <div className="relative flex flex-col items-center justify-end gap-24px">
+            <h4 className="text-h4 font-bold">{title}</h4>
+            <p className="text-sm font-bold text-button-secondary">
+              {formattedTime.formattedDate} {formattedTime.time}
+            </p>
 
-            <div className="absolute right-0 flex items-center gap-12px">
+            <div className="absolute right-0 flex items-center">
               <Button
                 type="button"
                 variant="secondaryBorderless"
@@ -51,53 +77,28 @@ const AnnouncementModal: React.FC = () => {
           </div>
 
           <div className="flex flex-col items-center gap-12px">
-            <div className="relative h-300px w-800px">
-              <Image src="/elements/kv_mask.png" alt="announcement sample" objectFit="cover" fill />
-            </div>
-
-            <p className="font-normal text-text-primary">
-              Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque
-              laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi
-              architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas
-              sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione
-              voluptatem sequi nesciunt. Sed ut perspiciatis unde omnis iste natus error sit
-              voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab
-              illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo
-              enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia
-              consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro
-              quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed
-              quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat
-              voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis
-              suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum
-              iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur,
-              vel illum qui dolorem eum fugiat quo voluptas nulla pariatur? Neque porro quisquam
-              est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non
-              numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat
-              voluptatem. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium
-              doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis
-              et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia
-              voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos
-              qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum
-              quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi
-              tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad
-              minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut
-              aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea
-              voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum
-              fugiat quo voluptas nulla pariatur? Ut enim ad minima veniam, quis nostrum
-              exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi
-              consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam
-              nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla
-              pariatur?
-            </p>
+            {isShowImage}
+            <p className="text-base font-normal text-text-primary">{content}</p>
           </div>
         </div>
 
         <div className="flex items-center justify-between">
-          <Button type="button" variant="secondaryBorderless" size="small">
+          <Button
+            type="button"
+            variant="secondaryBorderless"
+            size="small"
+            className="gap-spacing-3xs"
+          >
             <FiShare2 size={20} />
             <p>Share</p>
           </Button>
-          <Button type="button" variant="secondary" size="small">
+          <Button
+            type="button"
+            variant="secondary"
+            size="small"
+            onClick={visibleHandler}
+            className="px-24px py-8px"
+          >
             Close
           </Button>
         </div>
--- src/components/common/button.tsx
+++ src/components/common/button.tsx
@@ -3,34 +3,37 @@ import React from 'react';
 import { forwardRef } from 'react';
 import { cva, type VariantProps } from 'class-variance-authority';
 
-const buttonVariants = cva('group rounded-full font-bold flex items-center', {
-  variants: {
-    variant: {
-      primary:
-        'text-text-invert bg-button-primary enabled:hover:bg-button-primary-hover disabled:bg-button-disable',
-      primaryBorderless:
-        'text-text-brand enabled:hover:text-button-primary-hover disabled:text-text-note',
-      secondary:
-        'text-text-invert bg-button-secondary enabled:hover:bg-button-secondary-hover disabled:bg-button-disable',
-      secondaryBorderless:
-        'text-text-primary enabled:hover:text-button-primary disabled:text-button-disable',
-      accent:
-        'text-text-invert bg-button-accent enabled:hover:bg-button-accent-hover disabled:bg-button-disable',
-      accentBorderless:
-        'text-button-accent enabled:hover:text-button-accent-hover disabled:text-button-disable',
+const buttonVariants = cva(
+  'group rounded-full font-bold text-center justify-center flex items-center',
+  {
+    variants: {
+      variant: {
+        primary:
+          'text-text-invert bg-button-primary enabled:hover:bg-button-primary-hover disabled:bg-button-disable',
+        primaryBorderless:
+          'text-text-brand enabled:hover:text-button-primary-hover disabled:text-text-note',
+        secondary:
+          'text-text-invert bg-button-secondary enabled:hover:bg-button-secondary-hover disabled:bg-button-disable',
+        secondaryBorderless:
+          'text-text-primary enabled:hover:text-button-primary disabled:text-button-disable',
+        accent:
+          'text-text-invert bg-button-accent enabled:hover:bg-button-accent-hover disabled:bg-button-disable',
+        accentBorderless:
+          'text-button-accent enabled:hover:text-button-accent-hover disabled:text-button-disable',
+      },
+      size: {
+        small: 'p-8px text-sm',
+        medium: 'px-48px py-12px text-base',
+        large: 'px-54px py-18px text-lg',
+        icon: 'p-18px',
+      },
     },
-    size: {
-      small: 'px-24px py-12px text-sm',
-      medium: 'px-48px py-12px text-base',
-      large: 'px-54px py-18px text-lg',
-      icon: 'p-18px',
+    defaultVariants: {
+      variant: 'primary',
+      size: 'medium',
     },
-  },
-  defaultVariants: {
-    variant: 'primary',
-    size: 'medium',
-  },
-});
+  }
+);
 
 interface IButtonProps
   extends React.ButtonHTMLAttributes<HTMLButtonElement>,
