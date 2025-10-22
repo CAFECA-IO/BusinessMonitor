263a0d029af0533da0a030bbcd31347f886d545c
Luphia Chang <luphia@mermer.cc>
2025-10-22 20:15:56 +0800

    Update auth_context.tsx

--- src/contexts/auth_context.tsx
+++ src/contexts/auth_context.tsx
@@ -48,10 +48,10 @@ export function AuthProvider({ children }: IAuthProviderProps) {
         }
 
         const userData = await res.json();
-        logger.debug('Fetched user data:', userData);
+        logger.debug(`Fetched user data: ${userData}`);
         setUser(userData.payload as IdentityAccount);
       } catch (error) {
-        logger.warn('Auth check/fetch failed, logging out.', { error: String(error) });
+        logger.warn(`Auth check/fetch failed, logging out. ${{ error: String(error) }}`);
         logout();
       }
     },
