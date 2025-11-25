b58887dcad1725d28f0bac93662da766e80ca779
Luphia Chang <luphia@mermer.cc>
2025-11-25 20:02:08 +0800

    Update deploy_factory.ts

--- ignition/modules/deploy_factory.ts
+++ ignition/modules/deploy_factory.ts
@@ -4,11 +4,11 @@ import EntryPointModule from './deploy_entry_point';
 // Info: (20251125 - Tzuhan) 定義 SCWFactory 部署模組
 const SCWFactoryModule = buildModule('SCWFactoryModule', (m) => {
   // Info: (20251125 - Tzuhan) 1. 取得 EntryPoint 依賴
-  // 確保 EntryPoint 先部署，並取得其地址
+  // Info: (20251125 - Tzuhan) 確保 EntryPoint 先部署，並取得其地址
   const { entryPoint } = m.useModule(EntryPointModule);
 
   // Info: (20251125 - Tzuhan) 2. 部署 SCWFactory
-  // 將固定的 EntryPoint 地址傳入 Factory 的建構子
+  // Info: (20251125 - Tzuhan) 將固定的 EntryPoint 地址傳入 Factory 的建構子
   const factory = m.contract('SCWFactory', [entryPoint]);
 
   return { factory };
